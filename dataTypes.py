c7537ca797e1aa954bbabb4db2b9da51e5566cba
Marcell Gentles <marcellgentles@gmail.com>
2024-04-05 13:23:59 -0700

    added section and schedule classes and sorted schedule generator

--- dataTypes.py
+++ dataTypes.py
@@ -1,46 +1,138 @@
-class Schedule:
-    pass
+from datetime import time
 
-class CourseSection:
+# 0 for mandatory. The lowest numbers are weighted way more
+PRIORITY_D = {'ENGR079' : 0,
+              'ENGR079P' : 0,
+              'MATH055' : 1,
+              'CSCI070' : 1,
+              'LGCS010' : 2,
+              'MATH056' : 3,
+              'PHYS050' : 4,
+              'ENGR085' : 5}
+
+class Schedule:
+    """
+    Represents a class schedule, with a list of sections,
+    a prioritization score, and start and end times
+    """
+    def __init__(self, sections=[], score=0,
+                 startTime=None, endTime=None):
+        self.sections: list[Section] = sections
+        self.score: float = score
+        self.startTime: time = startTime
+        self.endTime: time = endTime
+        for section in sections:
+            self.add(section, inplace=True)
+
+    def __repr__(self, rich=False):
+        s = ''
+        s += 'Schedule with score {:.2f}, start time {}, and end time {}\n' \
+        .format(self.score, self.startTime.isoformat('minutes'),
+                self.endTime.isoformat('minutes'))
+        if rich:
+            for section in self.sections:
+                s += str(section) + ' ' + ''.join(section.days) + ' ' \
+                    + section.startTime.isoformat('minutes') \
+                        + ' - ' + section.endTime.isoformat('minutes') + '\n'
+        return s[:-1] # remove the last newline
+    
+    def richSummary(self):
+        return self.__repr__(rich=True)
+
+    def copy(self):
+        """Returns a copy of the schedule"""
+        return Schedule(self.sections.copy(), self.score,
+                        self.startTime, self.endTime)
+    
+    def add(self, section, *, inplace=False):
+        """Adds a section to the schedule"""
+        if inplace:
+            schedule = self
+        else:
+            schedule = self.copy()
+            if section not in schedule.sections:
+                schedule.sections.append(section)
+                if not schedule.startTime:
+                    schedule.startTime = section.startTime
+                    schedule.endTime = section.endTime
+                else:
+                    schedule.startTime = min(schedule.startTime, section.startTime)
+                    schedule.endTime = max(schedule.endTime, section.endTime)
+                schedule.updateScore()
+        return schedule
+
+    def remove(self, code, *, inplace=False):
+        pass
+
+    def updateScore(self):
+        """Updates the prioritazation score"""
+        self.score = 0
+        for section in self.sections:
+            if PRIORITY_D[section.course] != 0:
+                self.score += float(1 / PRIORITY_D[section.course])
+
+    # def conflictsWith(self, section: Section):
+    #     """Alternate way to check for conflicts"""
+    #     return section.conflictsWith(self)
+
+class Section:
+    """
+    Represents a section of a course, with course code, section number
+    (and campus if applicable), and meeting days and times
+    """
     def __init__(self, course, section, days, time) -> None:
-        self.course = course
-        self.section = section
-        self.days = days
-        self.startTime = time[0]
-        self.endTime = time[1]
+        self.course = course        # str
+        self.section = section      # str
+        self.days = days            # list[str]
+        self.startTime = time[0]    # time
+        self.endTime = time[1]      # time
     
     def __repr__(self):
-        return "-".join([self.course, self.section])
+        if len(self.section) < 3:
+            return "HM-".join([self.course, self.section])
+        else:
+            return self.course + self.section[0:2] + '-' + self.section[2:4]
     
     def isMandatory(self):
         # placeholder hard-code
-        return self.course in ('ENGR079', 'ENGR079P')
+        return PRIORITY_D[self.course] == 0
     
     def conflictsWith(self, other) -> bool:
         """
-        Returns true if the two classes have conflicting times
+        Returns true if the calling section conflicts with the schedule
         """
-        # first check days
-        sameDay = False
-        for selfDay in self.days:
-            for otherDay in other.days:
-                if selfDay == otherDay:
-                    sameDay = True
+        pass
+        if type(other) == Section:
+            # first check days
+            sameDay = False
+            for selfDay in self.days:
+                for otherDay in other.days:
+                    if selfDay == otherDay:
+                        sameDay = True
+                        break
+                if sameDay:
                     break
-            if sameDay:
-                break
-        if not sameDay:
+            if not sameDay:
+                return False
+            
+            # then times
+            if self.startTime >= other.startTime \
+                and self.startTime <= other.endTime:
+                return True
+            if other.startTime >= self.startTime \
+                and other.startTime <= self.endTime:
+                return True
             return False
-        # then times
-        selfStart, selfEnd, otherStart, otherEnd = \
-            map(int,
-                [self.startTime, self.endTime,
-                 other.startTime, other.endTime]
-                )
-        return (
-            selfStart in range(otherStart, otherEnd)
-            or selfEnd in range(otherStart, otherEnd)
-        )
+        
+        elif type(other) == Schedule:
+            for section in other.sections:
+                if self.conflictsWith(section):
+                    return True
+            return False
+        
+        else:
+            raise TypeError("Argument was not a Section or a schedule but a "
+                            + str(type(other)))
     
-    def isDuplicate(self, other) -> bool:
-        return self.course == other.course
+    def isRedundant(self, schedule: Schedule) -> bool:
+        return self.course in [sec.course for sec in schedule.sections]
--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,11 @@
 from dataTypes import *
 from io import TextIOWrapper
+import datetime
 
-def readSections(f: TextIOWrapper) -> list[CourseSection]:
+def readSections(f: TextIOWrapper) -> dict[str, list[Section]]:
     """
-    Turn text data into a list of CourseSection objects
+    Turn text data into a dictionary of lists of Sections of the same course
+    keyed by their code
     Text format:
     course
         section days startTime-endTime
@@ -13,41 +15,64 @@ def readSections(f: TextIOWrapper) -> list[CourseSection]:
         02 MW 1100-1215
         03 MW 1445-1600
     """
-    sections = []
+    sections = {}
     line = f.readline()
     while line != "":   # otherwise EOF
         if line != '\n':    # otherwise empty line
             if line[0] != ' ':  # course title with no indent
                 course = line.strip()
+                sections[course] = []
             else:   # indented section line under course line
                 sectionData = line.split()
                 section = sectionData[0]
                 days = [day for day in sectionData[1]]
-                time = [sectionData[2][:4], sectionData[2][5:9]]
-                sections.append(CourseSection(course, section, days, time))
+                # turn the string times into time objects
+                startTime = sectionData[2][:4]
+                startTime = datetime.time(int(startTime[0:2]), int(startTime[2:4]))
+                endTime = sectionData[2][5:9]
+                endTime = datetime.time(int(endTime[0:2]), int(endTime[2:4]))
+                time = [startTime, endTime]
+                sections[course].append(Section(course, section, days, time))
         line = f.readline()
     return sections
 
-def getScore(schedule: Schedule) -> int:
-    """
-    Return a score indicating how well a course aligns with priorities
-    """
-    pass
-
-def makeSchedules(sections: list[CourseSection], length) -> list[Schedule]:
+def makeSchedules(existingSchedule: Schedule,
+                  coursesToAdd: list[list[Section]]) -> list[Schedule]:
     """
     Make a list of schedules that contain each course no more than one time.
-    Each schedule must include sections of required courses.
     """
-    # build around mandatory courses, i.e. first add mandatory courses
-    pass
+    if coursesToAdd == []:
+        return [existingSchedule]
+    
+    newSchedules = []
+    for section in coursesToAdd[0]: # from the current course group to add
+        if not section.conflictsWith(existingSchedule):
+            newSchedules += makeSchedules(existingSchedule.add(section),
+                                          coursesToAdd[1:])
+    # we also consider not adding the course, unless mandatory
+    if not section.isMandatory():
+        newSchedules += makeSchedules(existingSchedule, coursesToAdd[1:])
+    return newSchedules
+
+def getSortedSchedules():
+    f = open('courseData.txt', 'r')
+    sections = list(readSections(f).values())
+    f.close()
+
+    blankSchedule = Schedule()
+    schedules = makeSchedules(blankSchedule, sections)
+    schedules.sort(key = lambda sched : sched.score, reverse = True)
+
+    return schedules
 
 def main():
-    """
-    For testing
-    """
-    f = open('courseData.txt', 'r')
-    print(readSections(f))
-    f.close()
+    schedules = getSortedSchedules()
+    perfectScore = schedules[0].score
+    print("Number of perfect schedules: {}\n".format(len([schedule for schedule in schedules
+                                                        if schedule.score == perfectScore])))
+    for schedule in getSortedSchedules()[:100]:
+        print(schedule.richSummary())
+        print()
+    
 
 main()
